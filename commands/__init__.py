380551a6095578b35cfb03bfa9433d20b1d8b31d
Alexandar <alexandarstrachinsky@gmail.com>
2026-05-16 15:01:44 +0300

    Доюавлена Vosk ru модель и comands/__init__ подготовлен для браузера

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,7 +1,11 @@
 # 🔹 Явная регистрация команд. PyInstaller гарантированно увидит эти импорты.
 from .system import SystemCommand
+#from .browse import BrowserCommand
 
 # Добавляй новые команды сюда:
 # from .browser import BrowserCommand
 
-COMMAND_CLASSES = [SystemCommand]
+COMMAND_CLASSES = [
+    SystemCommand,
+   # BrowserCommand,
+    ]
